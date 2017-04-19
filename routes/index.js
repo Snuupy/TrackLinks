a2f3d685b12d5daba0f8c209ca627b1030a7a6c2
Snuupy <snuupy[-removethis-]@snusite.com>
2017-04-19 05:01:43 -0700

    serves a real 1x1 pixel gif now and sets IDs at /set/id

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,27 @@ var storage = require("../util/storage");
 var cookieParser = require("cookie-parser");
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-	console.log(storage);
-	res.render("index", { title: "Express" });
-});
+// router.get("/", function(req, res, next) {
+// 	console.log(storage);
+// 	res.render("index", { title: "Express" });
+// });
 
-router.get("/newID", function(req, res) {
-	var hosturl = req.get("host");
-
+router.get("/", function(req, res) {
+	var fullUrl = req.protocol + '://' + req.get('host');
 	var id = storage.createID();
-	var newGifLink = `${hosturl}/${id}/gif.gif`;
+
+	res.cookie('userid', id, { expires: new Date(Date.now() + 900000), httpOnly: true });
+
 	res.render("newID", {
 		id: id,
-		gifLink: newGifLink
+		trackingURL: `${fullUrl}/${id}`,
+		gifLink: `${fullUrl}/${id}/gif.gif`,
+		setURL: `${fullUrl}/set/${id}`
 	});
-	// res.cookie('userid', '1', { expires: new Date(Date.now() + 900000), httpOnly: true });
-
 });
 
 router.get("/:id/gif.gif", function(req, res) {
-
+	var logged = false;
 	// if you don't have an ID or if your ID doesn't match the link owner, log data.
 	if (!req.cookies.userid || req.cookies.userid !== req.params.id) {
 		storage.log(req.params.id, {
@@ -31,21 +32,45 @@ router.get("/:id/gif.gif", function(req, res) {
 			useragent: req.get("user-agent"),
 			date: new Date()
 		});
+		logged = true;
 	}
 
 	var log = storage.getUserLog(req.params.id);
+	// res.json({
+	// 	views: log.length,
+	// 	log: log
+	// }); //TODO: should send a pixel gif
+	var buf = new Buffer([
+    0x47, 0x49, 0x46, 0x38, 0x39, 0x61, 0x01, 0x00, 0x01, 0x00, 
+    0x80, 0x00, 0x00, 0xff, 0xff, 0xff, 0x00, 0x00, 0x00, 0x2c, 
+    0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x02, 
+    0x02, 0x44, 0x01, 0x00, 0x3b]);
+
+	res.writeHead(200, {'Content-Type': 'image/gif' });
+	res.end(buf, 'binary');
+});
+
+router.get("/:id", function(req, res) {
+	var log = storage.getUserLog(req.params.id);
+
 	res.json({
+		yourCookieID: req.cookies.userid,
 		views: log.length,
 		log: log
 	});
 });
 
-router.get("/:id", function(req, res) {
-	var log = storage.getUserLog(req.params.id);
-	res.json({
-		views: log.length,
-		log: log
-	});
+router.get("/set/:id", function(req, res) {
+	// set cookie
+	res.cookie('userid', req.params.id, { expires: new Date(Date.now() + 900000), httpOnly: true });
+	var fullURL = req.protocol + '://' + req.get('host');
+
+	res.send(`Your ID has been set to: ${req.params.id}.
+		<br/>
+		Any browser that visits this page will not be tracked.
+		<br/>
+		Visit <a href=${fullURL}/${req.params.id}>${fullURL}/${req.params.id}</a> to see who read your emails.
+		`);
 });
 
 module.exports = router;
--- util/storage.js
+++ util/storage.js
@@ -1,7 +1,5 @@
 var currentID = 0;
-var db = {
-	1: []
-};
+var db = {};
 
 // db will look like:
 // {
@@ -18,9 +16,11 @@ var storage = {
 	},
 	getUserLog: function(userid) {
 		console.log(db[userid]);
-		return (db[userid]);
-		
-		// return db.userid;
+		if (db[userid]) {
+			return db[userid];
+		} else {
+			throw Error("Invalid URL");
+		}
 	},
 	log: function(userid, trackingData) {
 		if (db[userid]) {
